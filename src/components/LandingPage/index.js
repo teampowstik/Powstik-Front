54c536091201c22f0e5959de9c399edd60c32e2c
Rizwan <rizwankuwait@gmail.com>
2022-05-29 16:31:17 +0530

    added component

--- src/components/LandingPage/index.js
+++ src/components/LandingPage/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 
 import Footer from "./Footer"
+import Varieties from './ProductsVarieties'
 
 const LandingPage = () => {
   return (
     <React.Fragment>
+        <Varieties/>
         <Footer/>
     </React.Fragment>
   )
