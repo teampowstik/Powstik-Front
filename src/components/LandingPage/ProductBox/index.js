685ca484ad784a549a32dcaf88f16fa5302574be
pranav-bhalerao <pranavrajesh.bhalerao2019@vitstudent.ac.in>
2022-06-30 18:06:36 +0530

    axios module added

--- src/components/LandingPage/ProductBox/index.js
+++ src/components/LandingPage/ProductBox/index.js
@@ -1,30 +1,9 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios'
+import React from 'react';
+
 import CardHeaders from '../cardHeaders';
 import CardSlider from '../SlickSlider';
 
 const ProductBox = () => {
-
-	const [geriatrics, setGeriatrics] = useState([]);
-	const [motherAndBaby, setMotherAndBaby] = useState([]);
-	const [metabolics, setMetabolics] = useState([]);
-
-	const fetchData = async () => {
-
-		const { data } = await axios(`http://powstik-backend-test-v1.azurewebsites.net/consultation/bycategory/Diabetes`, {
-			"method": "GET",
-			"mode": "cors",
-			headers: {
-				'Access-Control-Allow-Origin': "*"
-			}
-		})
-			.then((data) => console.log(data))
-	};
-
-	useEffect(() => {
-		fetchData();
-	});
-
 	return (
 		<div>
 			<CardHeaders
