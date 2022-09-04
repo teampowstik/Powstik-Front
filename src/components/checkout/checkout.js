5c1482516abec74aca1193fd8bf09b25e96fa15d
pranavvdesai <pranavdesai.psd@gmail.com>
2022-09-05 00:00:35 +0530

    feat: index of checkout

---
+++ src/components/checkout/checkout.js
@@ -0,0 +1,106 @@
+import React from "react";
+import styled from "styled-components";
+
+import { H1, P1 } from "../../util/StyledComponent/premadeComponent";
+import Header from "./../LandingPage/Header/Header.component";
+import { Wrapper } from "../../util/buttons/DownloadButton/DownloadButton.styles";
+import PriceDetails from "./priceDetails";
+import AddAddress from "./addAddress";
+import SelectAddress from "./selectAddress";
+import OrderOverview from "./orderOverview";
+import PaymentOption from "./paymentOption";
+const Checkout = () => {
+  const [lookingFor, setLookingFor] = React.useState("false");
+  const [boolean_options, setBoolean_options] = React.useState([
+    {
+      _id: 1,
+      name: "Name1",
+      value: "Name1",
+      phone: "1234567890",
+      address: "VIT",
+    },
+    {
+      _id: 2,
+      name: "Name2",
+      value: "Name2",
+      phone: "9832498237",
+      address: "Vellore",
+    },
+  ]);
+  return (
+    <React.Fragment>
+      <Header />
+      <Container>
+        <Wrapper1>
+          <Login>
+            <NestedLogin>
+              <H1>1</H1>
+              <NestedLogin2>
+                <H1>LOGIN</H1>
+                <P1>+81924940494</P1>
+              </NestedLogin2>
+            </NestedLogin>
+            <Wrapper
+              // onClick={AddCart}
+              color="#8bc34a"
+              background="white"
+              border="#8bc34a"
+              to="#"
+            >
+              CHANGE
+            </Wrapper>
+          </Login>
+          <SelectAddress
+            value={lookingFor}
+            options={boolean_options}
+            onChange={(newValue) => setLookingFor(newValue)}
+          />
+          <AddAddress />
+          <OrderOverview />
+          <PaymentOption />
+        </Wrapper1>
+        <Wrapper2>
+          <PriceDetails />
+        </Wrapper2>
+      </Container>
+    </React.Fragment>
+  );
+};
+
+export default Checkout;
+const Container = styled.div`
+  display: flex;
+  margin: 0 2rem;
+  padding-top: 2rem;
+`;
+
+const Wrapper1 = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+`;
+const Wrapper2 = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 60%;
+`;
+
+const Login = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  background-color: #e8f3db;
+  padding: 20px;
+  border-radius: 5px;
+`;
+const NestedLogin = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+const NestedLogin2 = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding-left: 20px;
+`;
